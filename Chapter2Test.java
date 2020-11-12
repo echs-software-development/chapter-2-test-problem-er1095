c6352d3a8471a10da372104961f7f90cf1fc7adb
EMILY RAMIREZ <er1095@students.necsd.net>
2020-11-12 15:11:39 +0000

    I finished the problem

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,15 @@
 
-
+import java.util.Scanner;
 public class Chapter2Test {
   public static void main(String[] args) {
+  // gets input from user
+  Scanner scan = new Scanner(System.in); 
+  System.out.println("Enter two integers:");
+  // i didnt know how to put the casting in 
+  int Int1 = scan.nextInt();
+  int Int2 = scan.nextInt(); 
+  // calculates average 
+  System.out.print("The average is: " + (Int1 + Int2) / 2.0);
   
   }
 }
